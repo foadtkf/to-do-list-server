2fda633b7b563f0842dc72155a188d3b61a9ad5f
foadtkf <fakhrulif@gmail.com>
2022-05-19 06:52:14 +0600

    action delete added

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function run() {
         products = await cursor.toArray();
       res.send(products);
     });
+    app.delete("/tasks/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+      });
 } finally {
 }
 }
