ad0ced14c74618e0794f49ea6aa0dcabda2e6fdb
foadtkf <fakhrulif@gmail.com>
2022-05-19 06:29:34 +0600

    server site connected with mongodb

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// paste the "full driver code example" from mongodb connect to cluster
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sq6of.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("todos").collection("task_list");
+    console.log("Connected");    
+    app.get("/tasks", async (req, res) => {
+      console.log("query", req.query);
+      const query = {};
+      const cursor = productCollection.find(query);
+      
+      let products;
+        products = await cursor.toArray();
+      res.send(products);
+    });
+} finally {
+}
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+    res.send("laptopdokan server is running");
+  });
+  app.listen(port, () => {
+    console.log("it is running on ", port);
+  });
+  // error occured again
