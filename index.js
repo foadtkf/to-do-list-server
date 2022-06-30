eb9a27174ce8d92cb40e0b20112d505b93e1d8c6
foadtkf <fakhrulif@gmail.com>
2022-06-30 22:25:31 +0600

    status update added

--- index.js
+++ index.js
@@ -56,6 +56,19 @@ async function run() {
         const result=await productCollection.updateOne(filter,updatedDoc,options)
         res.send(result)
       })
+      app.put('/tasksstat/:id',async(req,res)=>{
+        const id=req.params.id
+        const updatedProduct=req.body
+        const filter ={_id:ObjectId(id)}
+        const options = { upsert: true }
+        const updatedDoc={
+          $set:{
+            status: updatedProduct.status
+          }
+        }
+        const result=await productCollection.updateOne(filter,updatedDoc,options)
+        res.send(result)
+      })
 } finally {
 }
 }
